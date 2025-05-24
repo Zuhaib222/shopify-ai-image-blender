548f180f104d32fc9a9f1f4e88da626f72dce676
Zuhaib Naeem <zuhaibnaeemb@gmail.com>
2025-05-24 09:36:11 +0500

    Fix file structure and index.js route

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const path = require("path");
 
 module.exports = (req, res) => {
   const filePath = path.join(__dirname, "public", "index.html");
-  const html = fs.readFileSync(filePath, "utf8");
-  res.setHeader("Content-Type", "text/html");
-  res.status(200).send(html);
+  try {
+    const html = fs.readFileSync(filePath, "utf8");
+    res.setHeader("Content-Type", "text/html");
+    res.status(200).send(html);
+  } catch (err) {
+    res.status(500).send("Failed to load HTML");
+  }
 };
